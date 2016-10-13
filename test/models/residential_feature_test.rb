2d0610714fbb3499f5438680fa0772776a6ee7f3
Jessica Dussault <jdussault4@gmail.com>
2016-10-13 14:17:40 -0500

    renames residentual -> residential

--- test/models/residential_feature_test.rb
+++ test/models/residential_feature_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ResidentualFeatureTest < ActiveSupport::TestCase
+class ResidentialFeatureTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
