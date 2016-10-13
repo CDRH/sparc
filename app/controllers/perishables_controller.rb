78e22826a0b6955607dceae70812535be3ff29a2
Jessica Dussault <jdussault4@gmail.com>
2016-10-13 10:19:59 -0500

    drops perishables:object_structure

--- app/controllers/perishables_controller.rb
+++ app/controllers/perishables_controller.rb
@@ -1,7 +1,7 @@
 class PerishablesController < ApplicationController
 
   active_scaffold :perishable do |conf|
-    conf.columns = [:room, :strat, :features, :fs_number, :salmon_museum_number, :grid, :quad, :depth, :perishable_period, :sa_no, :artifact_type, :perishable_count, :artifact_structure, :comments, :other_comments, :storage_location, :object_structure, :exhibit_location, :record_key_no, :museum_lab_no, :field_date, :original_analysis]
+    conf.columns = [:room, :strat, :features, :fs_number, :salmon_museum_number, :grid, :quad, :depth, :perishable_period, :sa_no, :artifact_type, :perishable_count, :artifact_structure, :comments, :other_comments, :storage_location, :exhibit_location, :record_key_no, :museum_lab_no, :field_date, :original_analysis]
     conf.columns[:perishable_period].form_ui = :select
     conf.actions.swap :search, :field_search
   end
