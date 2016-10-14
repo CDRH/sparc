2111e939938aece0203ff4dc19c63f0d612d6277
Jessica Dussault <jdussault4@gmail.com>
2016-10-14 12:47:14 -0500

    starts to fill in some basic tests for unit - strata - features
    will need to come back and fix these
    had to comment out the controller test because it needs
    devise authentication first, and it wasn't working out

--- test/models/soil_test.rb
+++ test/models/soil_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class SoilTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @item = Soil.first
+  end
+
+  test "associations" do
+    # check generally that these respond with collections, not nil, etc
+    assert_not_empty @item.features
+    assert_not_empty @item.strata
+    assert_not_empty @item.units
+    assert @item.art_type
+  end
+
 end
