0032b25972e9b818fcc2657d6cbe75ccc20c4c49
Jessica Dussault <jdussault4@gmail.com>
2016-11-21 09:51:32 -0600

    in progress image ingest
    will need a lot of work, because it appears that the units have
    ranges like 130A-F which means either a lot more parsing / iterating
    or somebody will need to adjust the spreadsheet, and then the script
    will have to accommodate multiple units
    
    either way, the images spreadsheet is being actively updated so
    this will need to pull in a new one regardless
    
    search appears to be broken for images

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   tables = [
-    :lithic_inventories,
     :bone_inventories,
-    :ceramic_inventories,
     :bone_tools,
+    :ceramic_inventories,
     :eggshells,
     :features,
+    :images,
+    :lithic_inventories,
     :ornaments,
     :perishables,
     :room_types,
@@ -20,6 +21,7 @@ Rails.application.routes.draw do
 
   tables.each do |table|
     namespace :admin do
+      get '/', to: 'units#index'
       resources table do
         # as_routes is deprecated but for some reason its replacement isn't working for me
         as_routes
@@ -32,5 +34,4 @@ Rails.application.routes.draw do
   # activescaffold_extensions(:rooms)
 
   get '/', to: 'static#index', as: :home
-
 end
