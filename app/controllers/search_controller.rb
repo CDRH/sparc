90409b33580f12e429e136a81ccddcd74b37ed44
Jessica Dussault <jdussault4@gmail.com>
2016-11-28 12:53:16 -0600

    updates placeholder actions and view for zone addition

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,13 +2,10 @@ class SearchController < ApplicationController
 
   def unit
     @unit = Unit.where(:unit_no => params["id"]).first
-    @zone = @unit.unit_no.sub(/^0*/, "").sub(/[A-Z\/]*$/, "") if @unit
   end
 
   def zone
-    # pad the incoming id to look like 001
-    zone_like = params["id"].rjust(3, "0")
-    @units = Unit.where("unit_no LIKE ?", "#{zone_like}%")
+    @units = Unit.joins(:zone).where(:zone => params["id"])
   end
 
 end
