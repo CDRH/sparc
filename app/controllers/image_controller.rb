27ecb762f7c96b54727277db4f7f96de609d9519
Jessica Dussault <jdussault4@gmail.com>
2016-12-09 16:47:41 -0600

    changes image and unit searches so that they are similar to each other

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -11,11 +11,11 @@ class ImageController < ApplicationController
     )
     images = images.where("image_no LIKE ?", "%#{params['image_no']}%") if !params["image_no"].blank?
     images = images.where("comments LIKE ?", "%#{params['comments']}%") if !params["comments"].blank?
-    images = add_to_query(images, :image_assocnoegs, params["assocnoeg"], :image_assocnoeg, true)
+    images = add_to_query(images, :image_assocnoegs, params["associated_negative"], :image_assocnoeg, true)
     images = add_to_query(images, :image_boxes, params["box"], :image_box, true)
     images = add_to_query(images, :image_creators, params["creator"], :image_creator, true)
     images = add_to_query(images, :image_formats, params["format"], :image_format, false)
-    images = add_to_query(images, :image_human_remains, params["remain"], :image_human_remain, false)
+    images = add_to_query(images, :image_human_remains, params["human_remain"], :image_human_remain, false)
     images = add_to_query(images, :image_orientations, params["orientation"], :image_orientation, true)
     images = add_to_query(images, :image_qualities, params["quality"], :image_quality, true)
     images = add_to_query(images, :image_subjects, params["subject"], :image_subjects, false)
