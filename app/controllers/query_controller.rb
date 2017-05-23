93af1ba5f69bb7ea1d5fdea7c377fde1214c86fa
Jessica Dussault <jdussault4@gmail.com>
2017-05-23 13:35:28 -0500

    roughing in possible UI for bone tools and inventory

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -1,5 +1,20 @@
 class QueryController < ApplicationController
 
+  def bones
+    @subsection = "artifacts"
+    # filter / search population
+    @total_bt = BoneTool.count
+    @bt_type = BoneTool.uniq.pluck(:tool_type).sort
+    @bt_tpcd = BoneTool.uniq.pluck(:tool_type_code).sort
+    @bt_spcd = BoneTool.uniq.pluck(:species_code).sort
+
+    @total_bi = BoneInventory.count
+    @bi_enby = BoneInventory.uniq.pluck(:entby).sort
+    @bi_loca = BoneInventory.uniq.pluck(:location).sort
+    @bi_quad = BoneInventory.uniq.pluck(:quad).sort
+    @bi_sano = BoneInventory.uniq.pluck(:sano).sort
+  end
+
   def ceramics
     @subsection = "artifacts"
   end
