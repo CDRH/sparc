ddc976e316fbee1894637e4bd5ebccbb687ba809
Jessica Dussault <jdussault4@gmail.com>
2017-05-23 10:26:40 -0500

    setting up navigation, subnavigation, and subsubnavigation...!

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -1,2 +1,27 @@
 class QueryController < ApplicationController
+
+  def ceramics
+    @subsection = "artifacts"
+  end
+
+  def lithics
+    @subsection = "artifacts"
+  end
+
+  def faunal
+    @subsection = "artifacts"
+  end
+
+  def perishables
+    @subsection = "artifacts"
+  end
+
+  def ornaments
+    @subsection = "artifacts"
+  end
+
+  def wood
+    @subsection = "artifacts"
+  end
+
 end
