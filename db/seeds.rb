e4e7da0cae2d83e0a9039e6f131401b0f3dda559
Greg Tunink <techgique@gmail.com>
2017-05-26 08:00:22 -0500

    Group sheet list & seed methods like seed control

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,26 @@
 ################
 
 files = {
-  bone_inventory: 'xls/BoneInventory2016-CCH.xlsx',
-  bonetools: 'xls/Bone_tool_DB.xlsx',
-  ceramic_inventory: 'xls/CeramicInventory2016.xlsx',
-  eggshells: 'xls/Eggshell_CCHedits.xls',
+  # Primary Tables
+  units: 'xls/Unit_Summary_CCHedits.xlsx',
+  strata: 'xls/Strata.xls',
   features: 'xls/Features_CCHedits.xls',
-  images: 'xls/Images.xlsx',
+
+  # Inventory Tables
+  bone_inventory: 'xls/BoneInventory2016-CCH.xlsx',
+  ceramic_inventory: 'xls/CeramicInventory2016.xlsx',
   lithic_inventory: 'xls/LithicInventory2016.xlsx',
+
+  # Analysis Tables
+  bonetools: 'xls/Bone_tool_DB.xlsx',
+  eggshells: 'xls/Eggshell_CCHedits.xls',
   ornaments: 'xls/Ornament_DB_CCHedits.xlsx',
   perishables: 'xls/Perishables-CCHedits-Nov13-2016.xls',
   select_artifacts: 'xls/Select_Artifacts.xls',
   soils: 'xls/Soil_Master.xlsx',
-  strata: 'xls/Strata.xls',
-  units: 'xls/Unit_Summary_CCHedits.xlsx',
+
+  # Images
+  images: 'xls/Images.xlsx'
 }
 
 # will contain an array of hashes
@@ -175,6 +182,10 @@ def select_or_create_zone_from_unit unit_str, spreadsheet, log=true
     return Zone.where(:number => num).first
   end
 end
+
+##################
+# PRIMARY TABLES #
+##################
 
 #########
 # Units #
@@ -393,6 +404,149 @@ def seed_features files
   end
 end
 
+####################
+# INVENTORY TABLES #
+####################
+
+##################
+# Bone Inventory #
+##################
+def seed_bone_inventory files
+  puts "Loading Bone Inventory ..."
+
+  s = Roo::Excelx.new(files[:bone_inventory])
+
+  s.sheet('Sheet1').each do |entry|
+    row = convert_empty_to_none(entry)
+
+    if row[0] != 'SITE'
+      bone_inventory = {}
+      bone_inventory[:site] = row[0]
+      bone_inventory[:box] = row[1]
+      bone_inventory[:fs] = row[2]
+      bone_inventory[:bone_inventory_count] = row[3]
+      bone_inventory[:gridew] = row[6]
+      bone_inventory[:gridns] = row[7]
+      bone_inventory[:quad] = row[8]
+      bone_inventory[:exactprov] = row[9]
+      bone_inventory[:depthbeg] = row[10]
+      bone_inventory[:depthend] = row[11]
+      bone_inventory[:stratalpha] = row[12]
+      bone_inventory[:strat_one] = row[13]
+      bone_inventory[:strat_two] = row[14]
+      bone_inventory[:othstrats] = row[15]
+      bone_inventory[:field_date] = row[16]
+      bone_inventory[:excavator] = row[17]
+      bone_inventory[:art_type] = row[18]
+      bone_inventory[:sano] = row[19]
+      bone_inventory[:recordkey] = row[20]
+      bone_inventory[:comments] = row[22]
+      bone_inventory[:entby] = row[23]
+      bone_inventory[:location] = row[24]
+
+      unit = select_or_create_unit(row[4], 'bone_inventories')
+      # bone_inventory[:room] = unit.unit_no
+
+      bone_inventory_row = BoneInventory.create(bone_inventory)
+      associate_strata_features(unit, row[5], row[21], bone_inventory_row, "bone_inventory")
+    end
+  end
+end
+
+#####################
+# Ceramic Inventory #
+#####################
+def seed_ceramic_inventory files
+  puts "Loading Ceramic Inventory ..."
+
+  s = Roo::Excelx.new(files[:ceramic_inventory])
+
+  s.sheet('Sheet1').each do |entry|
+    row = convert_empty_to_none(entry)
+
+    if row[0] != 'SITE'
+      ceramic_inventory = {}
+      ceramic_inventory[:site] = row[0]
+      ceramic_inventory[:box] = row[1]
+      ceramic_inventory[:fs] = row[2]
+      ceramic_inventory[:ceramic_inventory_count] = row[3]
+      ceramic_inventory[:gridew] = row[6]
+      ceramic_inventory[:gridns] = row[7]
+      ceramic_inventory[:quad] = row[8]
+      ceramic_inventory[:exactprov] = row[9]
+      ceramic_inventory[:depthbeg] = row[10]
+      ceramic_inventory[:depthend] = row[11]
+      ceramic_inventory[:stratalpha] = row[12]
+      ceramic_inventory[:strat_one] = row[13]
+      ceramic_inventory[:strat_two] = row[14]
+      ceramic_inventory[:othstrats] = row[15]
+      ceramic_inventory[:field_date] = row[16]
+      ceramic_inventory[:excavator] = row[17]
+      ceramic_inventory[:art_type] = row[18]
+      ceramic_inventory[:sano] = row[19]
+      ceramic_inventory[:recordkey] = row[20]
+      ceramic_inventory[:comments] = row[22]
+      ceramic_inventory[:entby] = row[23]
+      ceramic_inventory[:location] = row[24]
+
+      unit = select_or_create_unit(row[4], 'ceramic_inventories')
+      # ceramic_inventory[:room] = unit.unit_no
+
+      ceramic_inventory_row = CeramicInventory.create(ceramic_inventory)
+      associate_strata_features(unit, row[5], row[21], ceramic_inventory_row, "ceramic_inventory")
+    end
+  end
+end
+
+######################
+# Lithic Inventories #
+######################
+def seed_lithic_inventories files
+  puts "Loading Lithic Inventory ..."
+
+  s = Roo::Excelx.new(files[:lithic_inventory])
+
+  s.sheet('Sheet1').each do |entry|
+    row = convert_empty_to_none(entry)
+
+    if row[0] != 'SITE'
+      lithic = {}
+      lithic[:site] = row[0]
+      lithic[:box] = row[1]
+      lithic[:fs] = row[2]
+      lithic[:lithic_inventory_count] = row[3]
+      lithic[:gridew] = row[6]
+      lithic[:gridns] = row[7]
+      lithic[:quad] = row[8]
+      lithic[:exactprov] = row[9]
+      lithic[:depthbeg] = row[10]
+      lithic[:depthend] = row[11]
+      lithic[:stratalpha] = row[12]
+      lithic[:strat_one] = row[13]
+      lithic[:strat_two] = row[14]
+      lithic[:othstrats] = row[15]
+      lithic[:field_date] = row[16]
+      lithic[:excavator] = row[17]
+      lithic[:art_type] = row[18]
+      lithic[:sano] = row[19]
+      lithic[:recordkey] = row[20]
+      lithic[:comments] = row[22]
+      lithic[:entby] = row[23]
+      lithic[:location] = row[24]
+
+      unit = select_or_create_unit(row[4], 'lithic_inventories')
+      # lithic[:room] = unit.unit_no
+
+      lithic_row = LithicInventory.create(lithic)
+      associate_strata_features(unit, row[5], row[21], lithic_row, "lithic_inventory")
+    end
+  end
+end
+
+###################
+# ANALYSIS TABLES #
+###################
+
 ##############
 # Bone Tools #
 ##############
@@ -494,110 +648,6 @@ def seed_eggshells files
   end
 end
 
-#########
-# Soils #
-#########
-def seed_soils files
-  puts "Loading Soils..."
-
-  s = Roo::Excelx.new(files[:soils])
-
-  columns = {
-    # Ordered as seen in spreadsheet
-    # Comment columns not used or saved in database
-    site: "SITE",
-    room: "ROOM",
-    strat: "STRATUM",
-    feature_key: "FEATURE",
-    fs: "FS",
-    box: "BOX",
-    period: "PERIOD",
-    soil_count: "COUNT",
-    gridew: "GRIDEW",
-    gridns: "GRIDNS",
-    quad: "QUAD",
-    exactprov: "EXACTPROV",
-    depthbeg: "DEPTHBEG",
-    depthend: "DEPTHEND",
-    otherstrat: "OTHSTRATS",
-    date: "DATE",
-    excavator: "EXCAVATOR",
-    arttype: "ARTTYPE",
-    sample_no: "SAMPLE NO",
-    comments: "COMMENTS",
-    data_entry: "DATA ENTRY",
-    location: "LOCATION",
-  }
-
-  s.sheet('Sheet1').each(columns) do |row_hash|
-    soil = convert_empty_hash_values_to_none(row_hash)
-
-    # Skip header row
-    next if soil[:site] == "SITE"
-
-    soil[:art_type_id] = create_if_not_exists(ArtType, :art_type, soil[:arttype]).id
-
-    unit = select_or_create_unit(:room, 'soils')
-    soil[:room] = unit.unit_no
-
-    # Delete columns used but not saved in database
-    soil.delete :arttype
-
-    soil_record = Soil.create(soil)
-    associate_strata_features(unit, soil[:room], soil[:strat], soil_record, "soil")
-  end
-end
-
-###############
-# Perishables #
-###############
-# NOTE:  Perishables may belong to more than one unit
-def seed_perishables files
-  puts "Loading Perishables..."
-
-  s = Roo::Excel.new(files[:perishables])
-
-  s.sheet('all').each do |entry|
-    # empty fields should say "none" to standardize
-    row = convert_empty_to_none(entry)
-
-    if row[0] != 'FS Number'
-      perish = {}
-      perish[:artifact_structure] = row[12]
-      perish[:artifact_type] = row[10]
-      perish[:comments] = row[13]
-      perish[:depth] = row[6]
-      # TODO not sure what column to use for this
-      perish[:exhibit_location] = row[16]
-      perish[:field_date] = row[19]
-      perish[:fs_number] = row[0]
-      perish[:grid] = row[4]
-      perish[:museum_lab_no] = row[18]
-      perish[:original_analysis] = row[20]
-      perish[:other_comments] = row[14]
-      perish[:perishable_count] = row[11]
-      perish[:quad] = row[5]
-      perish[:record_key_no] = row[17]
-      perish[:room] = row[2]
-      perish[:sa_no] = row[9]
-      perish[:salmon_museum_number] = row[1]
-      perish[:storage_location] = row[15]
-      perish[:strat] = row[3]
-
-      # period
-      perish[:perishable_period_id] = create_if_not_exists(PerishablePeriod, :period, row[8]).id
-
-      perish_row = Perishable.create(perish)
-
-      units = row[2].split(/[,;]/).map { |u| u.strip }
-      units.each do |unit_str|
-        unit = select_or_create_unit(unit_str, "perishables")
-        associate_strata_features(unit, row[3], row[7], perish_row, "perishables")
-      end
-    end
-  end
-end
-
 #############
 # Ornaments #
 #############
@@ -651,6 +701,55 @@ def seed_ornaments files
   end
 end
 
+###############
+# Perishables #
+###############
+# NOTE:  Perishables may belong to more than one unit
+def seed_perishables files
+  puts "Loading Perishables..."
+
+  s = Roo::Excel.new(files[:perishables])
+
+  s.sheet('all').each do |entry|
+    # empty fields should say "none" to standardize
+    row = convert_empty_to_none(entry)
+
+    if row[0] != 'FS Number'
+      perish = {}
+      perish[:artifact_structure] = row[12]
+      perish[:artifact_type] = row[10]
+      perish[:comments] = row[13]
+      perish[:depth] = row[6]
+      # TODO not sure what column to use for this
+      perish[:exhibit_location] = row[16]
+      perish[:field_date] = row[19]
+      perish[:fs_number] = row[0]
+      perish[:grid] = row[4]
+      perish[:museum_lab_no] = row[18]
+      perish[:original_analysis] = row[20]
+      perish[:other_comments] = row[14]
+      perish[:perishable_count] = row[11]
+      perish[:quad] = row[5]
+      perish[:record_key_no] = row[17]
+      perish[:room] = row[2]
+      perish[:sa_no] = row[9]
+      perish[:salmon_museum_number] = row[1]
+      perish[:storage_location] = row[15]
+      perish[:strat] = row[3]
+
+      # period
+      perish[:perishable_period_id] = create_if_not_exists(PerishablePeriod, :period, row[8]).id
+
+      perish_row = Perishable.create(perish)
+
+      units = row[2].split(/[,;]/).map { |u| u.strip }
+      units.each do |unit_str|
+        unit = select_or_create_unit(unit_str, "perishables")
+        associate_strata_features(unit, row[3], row[7], perish_row, "perishables")
+      end
+    end
+  end
+end
 
 ####################
 # Select Artifacts #
@@ -706,138 +805,57 @@ def seed_select_artifacts files
   end
 end
 
-######################
-# Lithic Inventories #
-######################
-def seed_lithic_inventories files
-  puts "Loading Lithic Inventory ..."
+#########
+# Soils #
+#########
+def seed_soils files
+  puts "Loading Soils..."
 
-  s = Roo::Excelx.new(files[:lithic_inventory])
+  s = Roo::Excelx.new(files[:soils])
 
-  s.sheet('Sheet1').each do |entry|
-    row = convert_empty_to_none(entry)
+  columns = {
+    # Ordered as seen in spreadsheet
+    # Comment columns not used or saved in database
+    site: "SITE",
+    room: "ROOM",
+    strat: "STRATUM",
+    feature_key: "FEATURE",
+    fs: "FS",
+    box: "BOX",
+    period: "PERIOD",
+    soil_count: "COUNT",
+    gridew: "GRIDEW",
+    gridns: "GRIDNS",
+    quad: "QUAD",
+    exactprov: "EXACTPROV",
+    depthbeg: "DEPTHBEG",
+    depthend: "DEPTHEND",
+    otherstrat: "OTHSTRATS",
+    date: "DATE",
+    excavator: "EXCAVATOR",
+    arttype: "ARTTYPE",
+    sample_no: "SAMPLE NO",
+    comments: "COMMENTS",
+    data_entry: "DATA ENTRY",
+    location: "LOCATION",
+  }
 
-    if row[0] != 'SITE'
-      lithic = {}
-      lithic[:site] = row[0]
-      lithic[:box] = row[1]
-      lithic[:fs] = row[2]
-      lithic[:lithic_inventory_count] = row[3]
-      lithic[:gridew] = row[6]
-      lithic[:gridns] = row[7]
-      lithic[:quad] = row[8]
-      lithic[:exactprov] = row[9]
-      lithic[:depthbeg] = row[10]
-      lithic[:depthend] = row[11]
-      lithic[:stratalpha] = row[12]
-      lithic[:strat_one] = row[13]
-      lithic[:strat_two] = row[14]
-      lithic[:othstrats] = row[15]
-      lithic[:field_date] = row[16]
-      lithic[:excavator] = row[17]
-      lithic[:art_type] = row[18]
-      lithic[:sano] = row[19]
-      lithic[:recordkey] = row[20]
-      lithic[:comments] = row[22]
-      lithic[:entby] = row[23]
-      lithic[:location] = row[24]
+  s.sheet('Sheet1').each(columns) do |row_hash|
+    soil = convert_empty_hash_values_to_none(row_hash)
 
-      unit = select_or_create_unit(row[4], 'lithic_inventories')
-      # lithic[:room] = unit.unit_no
+    # Skip header row
+    next if soil[:site] == "SITE"
 
-      lithic_row = LithicInventory.create(lithic)
-      associate_strata_features(unit, row[5], row[21], lithic_row, "lithic_inventory")
-    end
-  end
-end
+    soil[:art_type_id] = create_if_not_exists(ArtType, :art_type, soil[:arttype]).id
 
-##################
-# Bone Inventory #
-##################
-def seed_bone_inventory files
-  puts "Loading Bone Inventory ..."
+    unit = select_or_create_unit(:room, 'soils')
+    soil[:room] = unit.unit_no
 
-  s = Roo::Excelx.new(files[:bone_inventory])
+    # Delete columns used but not saved in database
+    soil.delete :arttype
 
-  s.sheet('Sheet1').each do |entry|
-    row = convert_empty_to_none(entry)
-
-    if row[0] != 'SITE'
-      bone_inventory = {}
-      bone_inventory[:site] = row[0]
-      bone_inventory[:box] = row[1]
-      bone_inventory[:fs] = row[2]
-      bone_inventory[:bone_inventory_count] = row[3]
-      bone_inventory[:gridew] = row[6]
-      bone_inventory[:gridns] = row[7]
-      bone_inventory[:quad] = row[8]
-      bone_inventory[:exactprov] = row[9]
-      bone_inventory[:depthbeg] = row[10]
-      bone_inventory[:depthend] = row[11]
-      bone_inventory[:stratalpha] = row[12]
-      bone_inventory[:strat_one] = row[13]
-      bone_inventory[:strat_two] = row[14]
-      bone_inventory[:othstrats] = row[15]
-      bone_inventory[:field_date] = row[16]
-      bone_inventory[:excavator] = row[17]
-      bone_inventory[:art_type] = row[18]
-      bone_inventory[:sano] = row[19]
-      bone_inventory[:recordkey] = row[20]
-      bone_inventory[:comments] = row[22]
-      bone_inventory[:entby] = row[23]
-      bone_inventory[:location] = row[24]
-
-      unit = select_or_create_unit(row[4], 'bone_inventories')
-      # bone_inventory[:room] = unit.unit_no
-
-      bone_inventory_row = BoneInventory.create(bone_inventory)
-      associate_strata_features(unit, row[5], row[21], bone_inventory_row, "bone_inventory")
-    end
-  end
-end
-
-#####################
-# Ceramic Inventory #
-#####################
-def seed_ceramic_inventory files
-  puts "Loading Ceramic Inventory ..."
-
-  s = Roo::Excelx.new(files[:ceramic_inventory])
-
-  s.sheet('Sheet1').each do |entry|
-    row = convert_empty_to_none(entry)
-
-    if row[0] != 'SITE'
-      ceramic_inventory = {}
-      ceramic_inventory[:site] = row[0]
-      ceramic_inventory[:box] = row[1]
-      ceramic_inventory[:fs] = row[2]
-      ceramic_inventory[:ceramic_inventory_count] = row[3]
-      ceramic_inventory[:gridew] = row[6]
-      ceramic_inventory[:gridns] = row[7]
-      ceramic_inventory[:quad] = row[8]
-      ceramic_inventory[:exactprov] = row[9]
-      ceramic_inventory[:depthbeg] = row[10]
-      ceramic_inventory[:depthend] = row[11]
-      ceramic_inventory[:stratalpha] = row[12]
-      ceramic_inventory[:strat_one] = row[13]
-      ceramic_inventory[:strat_two] = row[14]
-      ceramic_inventory[:othstrats] = row[15]
-      ceramic_inventory[:field_date] = row[16]
-      ceramic_inventory[:excavator] = row[17]
-      ceramic_inventory[:art_type] = row[18]
-      ceramic_inventory[:sano] = row[19]
-      ceramic_inventory[:recordkey] = row[20]
-      ceramic_inventory[:comments] = row[22]
-      ceramic_inventory[:entby] = row[23]
-      ceramic_inventory[:location] = row[24]
-
-      unit = select_or_create_unit(row[4], 'ceramic_inventories')
-      # ceramic_inventory[:room] = unit.unit_no
-
-      ceramic_inventory_row = CeramicInventory.create(ceramic_inventory)
-      associate_strata_features(unit, row[5], row[21], ceramic_inventory_row, "ceramic_inventory")
-    end
+    soil_record = Soil.create(soil)
+    associate_strata_features(unit, soil[:room], soil[:strat], soil_record, "soil")
   end
 end
 
