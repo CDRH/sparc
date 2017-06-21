3b9da8e83459571c8a37b496371f745040da5b19
Greg Tunink <techgique@gmail.com>
2017-06-21 08:26:48 -0500

    Implement Ceramics search w dynamic table handling
    Populates forms and handles generating search results dynamically
    based on each table's ActiveRecord columns and associations
    
    Will DRY up the code more after Karin's search re-style

--- app/helpers/query_helper.rb
+++ app/helpers/query_helper.rb
@@ -1,10 +1,13 @@
 module QueryHelper
-
-  def checked? value, paramlist
+  def checked?(value, paramlist)
     if paramlist.blank?
       return false
     else
       return paramlist.include?(value.to_s)
     end
+  end
+
+  def delimit(number)
+    number_with_delimiter(number)
   end
 end
