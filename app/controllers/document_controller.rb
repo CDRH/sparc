c02f957a53988adf04e732044c732ec62987c9d5
Jessica Dussault <jdussault4@gmail.com>
2017-06-22 10:51:42 -0500

    Moves document functionality available in two views
    If anybody has a better idea of where to put the duplicated controller
    code, let me know.  I figured it wasn't the world's biggest problem
    at this point, especially since there are a few minor differences
    "type" is being treated differently in the two URLs but it seems to work

--- app/controllers/document_controller.rb
+++ app/controllers/document_controller.rb
@@ -23,12 +23,12 @@ class DocumentController < ApplicationController
       @type = DocumentType.joins(:units).where(units: {unit_no: @unit}).sorted.first.name.parameterize("_")
     end
 
-    type_name = get_name(@type)
+    type_name = get_doc_type_name(@type)
     res = Document.joins(:document_type, :units)
       .where("units.unit_no = ?", @unit)
       .where("document_types.name = ?", type_name)
       .sorted
-    @result_num = res.size
+    @result_num_docs = res.size
     @res = res.paginate(:page => params[:page], :per_page => 20)
 
     # generate the color coded document type buttons
@@ -36,13 +36,4 @@ class DocumentController < ApplicationController
     @group_colors = possible_types.sorted.group_by(&:color)
   end
 
-  private
-
-  def get_name(type)
-    if type == "feature_record_significant_artifact"
-      "Feature Record (Significant Artifact)"
-    else
-      type.titleize
-    end
-  end
 end
