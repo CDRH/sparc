4d385185e385886609e3fa858c86a5ac9a49860d
Jessica Dussault <jdussault4@gmail.com>
2017-06-23 16:28:15 -0500

    pulls information out of select artifacts tables
    if no matching row found in inventory / analysis table
    creates new record with minimal information
    if matching row found then adds some information to it
    had to rename / create several columns across tables
    associates SA with features instead of strata

--- app/controllers/admin/perishables_controller.rb
+++ app/controllers/admin/perishables_controller.rb
@@ -1,7 +1,7 @@
 class Admin::PerishablesController < ApplicationController
 
   active_scaffold :perishable do |conf|
-    conf.columns = [:units, :strata, :features, :fs_no, :salmon_museum_number, :grid, :quad, :depth, :occupation, :sa_no, :artifact_type, :count, :artifact_structure, :comments, :comments_other, :storage_location, :exhibit_location, :record_field_key_no, :museum_lab_no, :field_date, :original_analysis]
+    conf.columns = [:units, :strata, :features, :fs_no, :salmon_museum_number, :grid, :quad, :depth, :occupation, :sa_no, :artifact_type, :count, :artifact_structure, :comments, :comments_other, :storage_location, :exhibit_location, :record_field_key_no, :museum_lab_no, :field_date, :original_analysis, :select_artifact_info]
     conf.columns[:occupation].form_ui = :select
     conf.columns[:units].form_ui = :record_select
     conf.columns[:strata].form_ui = :record_select
