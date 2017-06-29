fc55507f34a657e45f4db98c8cfd678e79485f82
Greg Tunink <techgique@gmail.com>
2017-06-29 17:54:09 -0500

    Clean up long lines, add vertical space in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,25 +3,35 @@ Rails.application.routes.draw do
   devise_for :users
 
   get '/', to: 'static#index', as: :home
-  # documents
+
+  # Documents
   get '/documents', to: 'document#index', as: :documents
   get '/documents/type/:type', to: 'document#type', as: :documents_type
-  get '/documents/unit/:unit(/:type)', to: 'document#unit', as: :documents_unit
-  # explore
+  get '/documents/unit/:unit(/:type)', to: 'document#unit',
+      as: :documents_unit
+
+  # Explore
   get '/explore', to: 'explore#index', as: :explore
   get '/explore/early-zoom', to: 'explore#early_zoom', as: :explore_e_zoom
-  get '/explore/early-zoom-links', to: 'explore#early_zoom_links', as: :explore_e_zoom_links
+  get '/explore/early-zoom-links', to: 'explore#early_zoom_links',
+      as: :explore_e_zoom_links
   get '/explore/late', to: 'explore#late', as: :explore_l
   get '/explore/late-zoom', to: 'explore#late_zoom', as: :explore_l_zoom
   get '/explore/units', to: 'explore#units', as: :units
   get '/explore/unit/:number', to: 'explore#unit_summary', as: :unit
-  get '/explore/unit/:number/overview', to: 'explore#unit_overview', as: :unit_overview
-  get '/explore/unit/:number/images', to: 'explore#unit_images', as: :unit_images
-  get '/explore/unit/:number/documents', to: 'explore#unit_documents', as: :unit_documents
-  get '/explore/unit/:number/strata', to: 'explore#unit_strata', as: :unit_strata
-  get '/explore/unit/:number/features', to: 'explore#unit_features', as: :unit_features
+  get '/explore/unit/:number/overview', to: 'explore#unit_overview',
+      as: :unit_overview
+  get '/explore/unit/:number/images', to: 'explore#unit_images',
+      as: :unit_images
+  get '/explore/unit/:number/documents', to: 'explore#unit_documents',
+      as: :unit_documents
+  get '/explore/unit/:number/strata', to: 'explore#unit_strata',
+      as: :unit_strata
+  get '/explore/unit/:number/features', to: 'explore#unit_features',
+      as: :unit_features
   get '/explore/zone/:number', to: 'explore#zone', as: :search_zone
-  # images
+
+  # Images
   get '/gallery', to: 'image#index', as: :gallery
   get '/gallery/:number', to: 'image#show', as: :image
 
@@ -30,7 +40,8 @@ Rails.application.routes.draw do
   get '/query/canned', to: 'query#canned', as: :query_canned
   get '/query/:category', to: 'query#category', as: :query_category
   get '/query/:category/:type(/:table)', to: 'query#form', as: :query_form
-  get '/query/:category/:type/:table/results', to: 'query#results', as: :query_results
+  get '/query/:category/:type/:table/results', to: 'query#results',
+      as: :query_results
 
   # placeholders
   get '/about', to: 'static#about', as: :about
@@ -78,5 +89,4 @@ Rails.application.routes.draw do
   end
 
   # activescaffold_extensions(:rooms)
-
 end
