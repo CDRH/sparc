25e7ae6886c39cfeffac156037d0034cd10441c5
Greg Tunink <techgique@gmail.com>
2017-07-05 18:28:31 -0500

    Remove stratum_id column from Eggshells
    Column isn't in spreadsheet or assigned in seeds.rb,
    so ActiveRecord must have been finding it through associations
    
    Remove because redundant to strat column
    and association to strata through feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630162118) do
+ActiveRecord::Schema.define(version: 20170705232016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -456,7 +456,6 @@ ActiveRecord::Schema.define(version: 20170630162118) do
   end
 
   create_table "eggshells", force: :cascade do |t|
-    t.integer  "stratum_id"
     t.string   "unit"
     t.string   "strat"
     t.string   "salmon_museum_no"
