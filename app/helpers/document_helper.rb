16495a33c07220dac7946ac92423b33bcf8fd67b
Jessica Dussault <jdussault4@gmail.com>
2018-03-27 14:24:29 -0500

    removes (seemingly?) unused JS libraries, moves code to own file
    moves longer ruby logic in views into helper
    calls new javascript file on load

--- app/helpers/document_helper.rb
+++ app/helpers/document_helper.rb
@@ -1,2 +1,18 @@
 module DocumentHelper
+
+  def get_data_uri
+    if params[:manifest].present?
+      params[:manifest]
+    else
+      if !@first_doc_type.nil?
+        documents_unit_path(params.merge({:format => :json, :type => @first_doc_type}).permit(:unit, :type, :format))
+      else
+        ""
+      end
+    end
+  end
+
+  def get_manifests
+    documents_unit_path(params.merge({:format => :json}).permit(:unit, :type))
+  end
 end
