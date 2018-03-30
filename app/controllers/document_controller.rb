a61f8ffb86b83930b53b8b212746088fe49e477d
Jessica Dussault <jdussault4@gmail.com>
2018-03-30 13:56:18 -0500

    refactors document unit controller
    revisits some logic for looking up doc types, units, etc
    comments code, renames vars, reindents, whitespace removal
    alphabetizes private methods on controller

--- app/controllers/document_controller.rb
+++ app/controllers/document_controller.rb
@@ -3,7 +3,6 @@ class DocumentController < ApplicationController
   def index
     # filter out units with no documents
     @units = Unit.includes(:documents).where.not(documents: { id: nil }).sorted
-
     @units_no_docs = Unit.includes(:documents).where(documents: { id: nil }).sorted
   end
 
@@ -16,99 +15,64 @@ class DocumentController < ApplicationController
   end
 
   def unit
-    @unit = params["unit"]
-    @type = nil
+    @unit_no = params["unit"]
+    @unit = Unit.find_by(unit_no: @unit_no)
+    @doc_type = nil
     @collections = {}
     @first_doc_type = nil
     if params["type"].present?
-      @type = params["type"]
+      @doc_type = params["type"]
     else
-      @type = DocumentType.joins(:units)
-                .where(units: {unit_no: @unit}).sorted.first
-                .name.parameterize(separator: "_")
-      DocumentType.all.each do |dt|
-        units = DocumentType.joins(:units).where(units: {unit_no: @unit}, name: dt.name).size
-        if units > 0
-          @collections[dt.name.parameterize(separator: "_")] = Document.joins(:document_type, :units)
-            .where("units.unit_no = ?", @unit)
-            .where("document_types.name = ?", dt.name)
-            .sorted
-          end
-          @first_doc_type = @collections[@collections.keys.first].first.document_type.name if @collections[@collections.keys.first]
+      # find the first document type for the requested unit
+      @doc_type = @unit.document_types.sorted.first
+                    .name.parameterize(separator: "_")
+      # get all relevant documents and organize into the collections by document type
+      # to build collection manifest for iiif
+      DocumentType.all.each_with_index do |dt, idx|
+        @first_doc_type = dt.name if idx == 0
+        key = dt.name.parameterize(separator: "_")
+        docs = Document.joins(:document_type, :units)
+                .where("units.unit_no = ?", @unit_no)
+                .where(document_type: dt)
+                .sorted
+        if docs.count > 0
+          @collections[key] = docs
+        end
       end
     end
 
-    type_name = get_doc_type_name(@type)
-    
-    @collections.each {|k,v| puts "#{k} = #{v.size}"}
+    # get documents to display using document type name
+    type_name = get_doc_type_name(@doc_type)
     res = Document.joins(:document_type, :units)
-            .where("units.unit_no = ?", @unit)
+            .where("units.unit_no = ?", @unit_no)
             .where("document_types.name = ?", type_name)
             .sorted
-            
+
     @result_num_docs = res.size
     @res = res.paginate(page: params[:page], per_page: 20)
 
-    # generate the color coded document type buttons
-    possible_types = DocumentType.joins(:units)
-                      .where("units.unit_no = ?", @unit).uniq
-    @group_colors = possible_types.sorted.group_by(&:color)
-    
     code = params['code'].present? ? params['code'] : nil
 
     respond_to do |format|
-        format.html
-        format.json { render :json => (@collections.size > 0 ? build_collection(@collections, @unit) : build_manifest(res, code)) }
-      end
-  end
-  
-  private
-  
-  require 'iiif/presentation'
-
-  def image_annotation_from_id(title, image_id, image_info)
-    annotation = IIIF::Presentation::Annotation.new
-    annotation.resource = image_resource_from_page_hash(image_id)
-    canvas = IIIF::Presentation::Canvas.new
-    # TODO what should this URL be? Use parameterize for title
-    canvas_uri = "http://example.org/#{title.gsub(' ','_')}/pages/#{image_id}"
-    canvas['@id'] = canvas_uri
-    canvas.label = image_info['label']
-    canvas.width = annotation.resource['width']
-    canvas.height = annotation.resource['height']
-    canvas.images << annotation
-    canvas.metadata = [
-      { 'label' => 'Page ID',
-        'value' => image_info['page']
-      },
-      { 'label' => 'Creator', 
-        'value' => image_info['creator']
-      },
-      { 'label' => 'Rights', 
-        'value' => image_info['rights']
+      format.html
+      format.json {
+        response = @collections.size > 0 ?
+          build_collection(@collections, @unit_no) :
+          build_manifest(res, code)
+        render :json => response
       }
-    ]
-    canvas
+    end
   end
 
-  def image_resource_from_page_hash(page_id)
-    base_uri = "#{SETTINGS["iiif_server"]}%2Fdocuments%2F#{page_id.gsub('/','%2F')}"
-    params = {service_id: base_uri}
-    begin
-      image_resource = IIIF::Presentation::ImageResource.create_image_api_image_resource(params)
-    rescue
-      base_uri = "#{SETTINGS["iiif_server"].gsub('sparc','coming_soon.jpg')}"
-      params = {service_id: base_uri}
-      image_resource = IIIF::Presentation::ImageResource.create_image_api_image_resource(params)
-    end
-    image_resource
-  end
+  private
+
+  require 'iiif/presentation'
 
   def build_collection(collection_hash, unit)
     wrapper = IIIF::Presentation::Collection.new('@id' => documents_unit_path({unit: unit}))
     collection = IIIF::Presentation::Collection.new('@id' => documents_unit_path({unit: unit}), '@label' => unit)
     collection_hash.each do |k, docs|
-      
+
       unit = docs.first.units.first
       title = unit.unit_no
       manifest = IIIF::Presentation::Manifest.new('@id' => documents_unit_path({unit: title, type: k, format: "json"}))
@@ -120,7 +84,7 @@ class DocumentController < ApplicationController
 
       collection.manifests <<  manifest
     end
-  
+
     wrapper.collections << collection
     wrapper.to_json(pretty: true)
   end
@@ -135,7 +99,7 @@ class DocumentController < ApplicationController
     # document type
     code = first_doc.page_id.split('_')[1]
     doc_type = DocumentType.find_by(code: code)
-    manifest.label = "#{room_type} #{title.split('-').first} #{doc_type}"
+    manifest.label = "#{room_type} #{title.split('-').first} #{doc_type.name}"
     manifest.viewing_hint = 'paged'
     manifest.metadata = [
       { 'label' => 'Unit', 'value' => "#{title}" },
@@ -164,6 +128,44 @@ class DocumentController < ApplicationController
     manifest.insert_after(existing_key: 'label', new_key: 'thumbnail', value: thumb)
 
     manifest.to_json(pretty: true)
+  end
+
+  def image_annotation_from_id(title, image_id, image_info)
+    annotation = IIIF::Presentation::Annotation.new
+    annotation.resource = image_resource_from_page_hash(image_id)
+    canvas = IIIF::Presentation::Canvas.new
+    # TODO what should this URL be? Use parameterize for title
+    canvas_uri = "http://example.org/#{title.gsub(' ','_')}/pages/#{image_id}"
+    canvas['@id'] = canvas_uri
+    canvas.label = image_info['label']
+    canvas.width = annotation.resource['width']
+    canvas.height = annotation.resource['height']
+    canvas.images << annotation
+    canvas.metadata = [
+      { 'label' => 'Page ID',
+        'value' => image_info['page']
+      },
+      { 'label' => 'Creator',
+        'value' => image_info['creator']
+      },
+      { 'label' => 'Rights',
+        'value' => image_info['rights']
+      }
+    ]
+    canvas
+  end
+
+  def image_resource_from_page_hash(page_id)
+    base_uri = "#{SETTINGS["iiif_server"]}%2Fdocuments%2F#{page_id.gsub('/','%2F')}"
+    params = {service_id: base_uri}
+    begin
+      image_resource = IIIF::Presentation::ImageResource.create_image_api_image_resource(params)
+    rescue
+      base_uri = "#{SETTINGS["iiif_server"].gsub('sparc','coming_soon.jpg')}"
+      params = {service_id: base_uri}
+      image_resource = IIIF::Presentation::ImageResource.create_image_api_image_resource(params)
+    end
+    image_resource
   end
 
 end
