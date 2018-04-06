0235b3ef873287bc9da33a2030e1f1d89c482cc4
Greg Tunink <techgique@gmail.com>
2018-04-06 01:27:37 -0500

    Add model-defined association abstraction
    Add ability to define model method `abstraction` to choose which column
    will be used for form inputs and results values when a table is
    associated with the table being searched and displayed
    
    Augment code to populate selects to use defined abstraction before
    falling back to default "name" column in an associated table
    
    When displaying values, first check for defined abstraction,
    then try fallback defaults like "name" or "(table)_no" columns
    
    Check for "id" column last, appending "(DB ID)" string to alert
    to define another column to display instead
    
    Last resort, explicitly indicate an associated table has no info to display
    to alert to define a column to display

--- app/lib/active_record_abstraction.rb
+++ app/lib/active_record_abstraction.rb
@@ -14,10 +14,18 @@ module ActiveRecordAbstraction
           res = Occupation.where(id: result[:occupation_id]).first.name
         elsif result.respond_to?(assoc_col)
           if result.send(assoc_col).present?
-            if result.send(assoc_col).respond_to?(:name)
+            if assoc_col.classify.constantize.respond_to?("abstraction")
+              abstraction = assoc_col.classify.constantize.send("abstraction")
+              res =
+                result.send(assoc_col).send(abstraction[:assoc_input_column])
+            elsif result.send(assoc_col).respond_to?(:name)
               res = result.send(assoc_col).name
+            elsif result.send(assoc_col).respond_to?("#{assoc_col}_no")
+              res = result.send(assoc_col).send("#{assoc_col}_no")
+            elsif result.respond_to?("id")
+              res = result.send(assoc_col).id.to_s << " (DB ID)"
             else
-              res = result.send(assoc_col).id
+              res = "Assoc Info Missed"
             end
           else
             res = "N/A"
@@ -25,13 +33,20 @@ module ActiveRecordAbstraction
         elsif result.respond_to?(assoc_col.pluralize)
           if result.send(assoc_col.pluralize).present?
             assoc_obj = result.send(assoc_col.pluralize)
-            if assoc_obj.first.respond_to?("name")
-              res = assoc_obj.map { |r| r.name }.join("; ")
-            elsif assoc_obj.first.respond_to?(assoc_col << "_no")
-              res = assoc_obj.map { |r| r.send(assoc_col << "_no") }
-                .join("; ")
+            if assoc_obj.first.respond_to?("abstraction")
+              abstraction = assoc_obj.send("abstraction")
+              res =
+                assoc_obj.map { |r| r.send(abstraction[:assoc_input_column]) }
+                .uniq.join("; ")
+            elsif assoc_obj.first.respond_to?("name")
+              res = assoc_obj.map { |r| r.name }.uniq.join("; ")
+            elsif assoc_obj.first.respond_to?("#{assoc_col}_no")
+              res = assoc_obj.map { |r| r.send("#{assoc_col}_no") }
+                .uniq.join("; ")
             elsif assoc_obj.first.respond_to?("id")
-              res = assoc_obj.map { |r| r.id }.join("; ")
+              res = assoc_obj.map { |r| r.id }.uniq.join("; ") << " (DB ID)"
+            else
+              res = "Pluralized Assoc Info Missed"
             end
           else
             res = "N/A"
@@ -43,7 +58,14 @@ module ActiveRecordAbstraction
         if column[/_(?:habtm|hm)$/]
           column = column[/^(.+)_(?:habtm|hm)$/, 1]
 
+          if column.classify.constantize.respond_to?("abstraction")
+            abstraction = column.classify.constantize.send("abstraction")
+            res = result.send(column)
+              .map { |r| r.send(abstraction[:assoc_input_column]) }
+              .uniq.sort.join("; ")
+          else
             res = result.send(column).map { |r| r.name }.uniq.sort.join("; ")
+          end
         elsif column[/_join$/]
           column = column[/^(.+)_join$/, 1]
           column, table = column.split("|")
