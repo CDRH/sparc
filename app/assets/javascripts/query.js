eb7885d16c47bc83a62f68586314f52e8d66618f
Greg Tunink <techgique@gmail.com>
2018-05-03 14:29:21 -0500

    Move collapse/expand JS to Query-specific JS file

--- app/assets/javascripts/query.js
+++ app/assets/javascripts/query.js
@@ -1,4 +1,29 @@
 // On page load
 $(function () {
+  // Enable Bootstrap popovers
   $('[data-toggle="popover"]').popover();
+
+  // Add collapse class on page load
+  $("#query_table_description").addClass('collapse');
+  $("#query_other_otions").addClass('collapse');
+
+  // Change text on "show table description" button
+  $("#query_desc_disp_btn").clicktoggle(
+    function() {
+      $(this).text("Hide table description");
+    },
+    function() {
+      $(this).text("Show table description");
+    }
+  );
+
+  // Change text on "show more search options"
+  $("#query_search_options_btn").clicktoggle(
+    function() {
+      $(this).text("Hide more search options");
+    },
+    function() {
+      $(this).text("Show more search options");
+    }
+  );
 })
