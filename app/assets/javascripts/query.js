58828c8796adc176b84fdb64ad9832de44e39f8d
Greg Tunink <techgique@gmail.com>
2018-05-03 16:30:25 -0500

    Add accessible badges indicating option selections

--- app/assets/javascripts/query.js
+++ app/assets/javascripts/query.js
@@ -1,7 +1,26 @@
 // On page load
 $(function () {
+  // =====================
+  // Common Search Options
+  // =====================
+
   // Enable Bootstrap popovers
   $('[data-toggle="popover"]').popover();
+
+  var optionGroups =
+    ['unitclass', 'unitnum', 'occupation', 'stratum', 'feature' ];
+
+  // Update option group badge counts
+  optionGroups.forEach(function(group) {
+    groupCheckboxes = '#collapse-'+ group +' .checkbox';
+    $(groupCheckboxes).each(function(index, checkbox) {
+      $(checkbox).click(group, updateBadgeCount);
+    });
+  });
+
+  // =====================
+  // Table Search Options
+  // =====================
 
   // Add collapse class on page load
   $("#query_table_description").addClass('collapse');
@@ -26,4 +45,29 @@ $(function () {
       $(this).text("Show more search options");
     }
   );
-})
+});
+
+// ====================
+// Function Definitions
+// ====================
+
+var updateBadgeCount = function(event) {
+  group = event.data;
+
+  checkboxes = '#collapse-'+ group +' .checkbox input:checked';
+  count = $(checkboxes).length;
+
+  badge = '#heading-'+ group +' .badge';
+  $(badge).text(count);
+
+  if (count > 0) {
+    if ($(badge).hasClass("hidden")) {
+      $(badge).removeClass("hidden");
+      $(badge).attr("aria-hidden", false);
+    }
+  }
+  else {
+    $(badge).addClass("hidden");
+    $(badge).attr("aria-hidden", true);
+  }
+};
