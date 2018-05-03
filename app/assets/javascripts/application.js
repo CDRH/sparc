f003246c2a010c6a60d9effaec3c2aa9ee4ad2be
Karin Dalziel <kdalziel2@unl.edu>
2018-05-03 09:13:42 -0500

    javascript show/hide on query page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,52 @@
 //= require lightbox_activation.js
 
 // require_tree .
+
+
+
+// Toggle function
+$.fn.clicktoggle = function(a, b) {
+  return this.each(function() {
+    var clicked = false;
+    $(this).bind("click", function() {
+      if (clicked) {
+        clicked = false;
+        return b.apply(this, arguments);
+      } else {
+        clicked = true;
+        return a.apply(this, arguments);
+      }
+    });
+  });
+};
+
+// ===========
+// Query Pages (needs click toggle above)
+// ===========
+
+$(function() {
+  // Add collapse class on page load
+  $("#query_table_description").addClass('collapse');
+  $("#query_other_otions").addClass('collapse');
+
+  // Change text on "show table description" button
+  $("#query_desc_disp_btn").clicktoggle(
+    function() {
+      $(this).text("Hide table description");
+    },
+    function() {
+      $(this).text("Show table description");
+    }
+  );
+
+  // change text on "show more search options"
+  $("#query_search_options_btn").clicktoggle(
+    function() {
+      $(this).text("Hide more search options");
+    },
+    function() {
+      $(this).text("Show more search options");
+    }
+  );
+
+});
