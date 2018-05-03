f5051841dd2d1c7f1a30f4c92ef4338e9ac26dda
Jessica Dussault <jdussault4@gmail.com>
2018-05-03 10:30:48 -0500

    instructions for about subpages

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,6 +4,10 @@ class StaticController < ApplicationController
     @section = "about"
   end
 
+  # this action uses part of the URL to determine which partial to pull in dynamically
+  # to add more about pages, simply create a partial with the name as you would like it to
+  # appear in the URL and add the page to the subnavigation
+
   def about_sub
     @section = "about"
     @subsection = params["name"]
