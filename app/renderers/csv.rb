4fc288e4b81fbfd9b0087810429a4f90f01e466d
Jessica Dussault <jdussault4@gmail.com>
2018-05-07 10:13:46 -0500

    implements occupation by association in csv
    would like to move this logic somewhere accessible by
    both view and csv but due to time constraints simply
    duplicating the logic for now :(
    would be happy to change push(exploding_array) to another
    syntax if there is a preferred form

--- app/renderers/csv.rb
+++ app/renderers/csv.rb
@@ -4,12 +4,26 @@ ActionController::Renderers.add :csv do |data, options|
 
   csv_data = CSV.generate(headers: true) do |csv|
     # Use specified column names if passed, otherwise use record field names
-    csv << options[:table_fields].map { |field| field_label(field) }
+
+    # add occupation column if not present
+    no_occupation = !@table_fields.map{ |f| f[:name] }.include?("occupation")
+
+    header = []
+    if options[:occupation_source].present? && no_occupation
+      header.push(options[:occupation_source])
+    end
+    labels = options[:table_fields].map { |field| field_label(field) }
+    csv << header.push(*labels)
 
     data.each do |result|
-      csv << options[:table_fields].map do |field|
+      row = []
+      if options[:occupation_source].present? && no_occupation
+        row.push(Occupation.where(id: result[:occupation_id]).first.name)
+      end
+      values = options[:table_fields].map do |field|
         display_value(result, field[:name], field[:assoc])
       end
+      csv << row.push(*values)
     end
   end
   send_data csv_data, filename: filename
