e33624c6b08a969bcd13ff48a3d282b0e322346f
Greg Tunink <techgique@gmail.com>
2026-02-12 14:28:30 -0600

    Fix asset manifest to work with dev and prod env

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,7 @@
 //= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets
-//= link_tree ../fonts
+//= link_tree ../javascripts
+//= link application.css
+//= link application.js
+
 //= link favicon/browserconfig.xml
+//= link maps.css
